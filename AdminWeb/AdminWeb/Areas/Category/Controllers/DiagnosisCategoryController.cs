5c19e88cb26d992c8b18eb071033a5164d18d516
pdkladykillah <khoapham3900@gmail.com>
2023-09-28 17:19:25 +0700

    final commit after fixed bug

--- AdminWeb/AdminWeb/Areas/Category/Controllers/DiagnosisCategoryController.cs
+++ AdminWeb/AdminWeb/Areas/Category/Controllers/DiagnosisCategoryController.cs
@@ -40,12 +40,6 @@ namespace AdminWeb.Areas.Category.Controllers
             return DataSourceLoader.Load(_response, loadOptions);
         }
 
-        [HttpGet]
-        public object GetDiagnosisCategoryById(int id, DataSourceLoadOptions loadOptions)
-        {
-            IEnumerable<DiagnosisCategoryResponseModel> _response = diagnosisCategoryList.Where(e => e.DiagnosisCategoryId == id).FirstOrDefault();
-            return DataSourceLoader.Load(_response, loadOptions);
-        }
 
         public static List<DiagnosisCategoryResponseModel> diagnosisCategoryList = new()
         {

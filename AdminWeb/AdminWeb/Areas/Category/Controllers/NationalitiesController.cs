e1dddf90d7a7cd5633d88240180cf4a254fa6580
pdkladykillah <khoapham3900@gmail.com>
2023-09-28 11:41:06 +0700

    Add Nationalites UI

---
+++ AdminWeb/AdminWeb/Areas/Category/Controllers/NationalitiesController.cs
@@ -0,0 +1,88 @@
+﻿using AdminWeb.Dtos.ApiResponse;
+using AdminWeb.Models;
+using AdminWeb.Services;
+using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Mvc;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace AdminWeb.Areas.Category.Controllers
+{
+    [Area("Category")]
+    public class NationalitiesController : Controller
+    {
+        private readonly IApiService _apiService;
+
+        public NationalitiesController(IApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        public IActionResult List()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        #region -- Methods --
+
+        [HttpGet]
+        public object GetNationalityList(DataSourceLoadOptions loadOptions)
+        {
+            var _response = NationalityList;
+
+            return DataSourceLoader.Load(_response, loadOptions);
+        }
+
+        public static List<NationalitiesResponseModel> NationalityList = new List<NationalitiesResponseModel>()
+        {
+            new NationalitiesResponseModel
+            {
+                NationalityId = 1,
+                NationalityCode = "VN",
+                NationalityName = "Việt Nam",
+                IsActive = true,
+                CreationDate = DateTime.Now,
+                CreatedBy = null,
+                LastUpdatedDate = DateTime.Now,
+                LastUpdatedBy = 1
+            },
+            new NationalitiesResponseModel
+            {
+                NationalityId = 2,
+                NationalityCode = "LA",
+                NationalityName = "America",
+                IsActive = true,
+                CreationDate = DateTime.Now,
+                CreatedBy = null,
+                LastUpdatedDate = null,
+                LastUpdatedBy = null
+            },
+            new NationalitiesResponseModel
+            {
+                NationalityId = 3,
+                NationalityCode = "AUS",
+                NationalityName = "Úc",
+                IsActive = true,
+                CreationDate = DateTime.Now,
+                CreatedBy = null,
+                LastUpdatedDate = null,
+                LastUpdatedBy = null
+            }
+
+            #endregion
+        };
+    }
+}

60984b445be634bb5a242c444f76291ba49f78c2
Dương Quốc Thái <quocthai261@gmail.com>
2023-09-29 17:13:46 +0700

    Fix commit

---
+++ AdminWeb/AdminWeb/Areas/Category/Controllers/PositionApplingsController.cs
@@ -0,0 +1,45 @@
+﻿using AdminWeb.Services;
+using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminWeb.Areas.Category.Controllers
+{
+    [Area("Category")]
+    public class PositionApplingsController : Controller
+    {
+        private readonly IApiService _apiService;
+
+        public PositionApplingsController(IApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        public IActionResult List()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+/*
+        [HttpGet]
+        public object GetBankList(DataSourceLoadOptions loadOptions)
+        {
+            var _response = bankList;
+
+            return DataSourceLoader.Load(_response, loadOptions);
+        }*/
+
+    }
+}
